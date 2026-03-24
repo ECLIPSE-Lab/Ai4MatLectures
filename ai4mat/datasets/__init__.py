c2f8f350b21c047faab21331b9dfe9d901b5782e
Philipp Pelz <philipp.pelz@fau.de>
2026-03-24 14:06:07 +0100

    feat: wire up public ai4mat.datasets API

--- ai4mat/datasets/__init__.py
+++ ai4mat/datasets/__init__.py
@@ -1,1 +1,17 @@
-# populated in Task 10 after all wrappers are implemented
+from .iris import IrisDataset
+from .digits import DigitsDataset
+from .tensile_test import TensileTestDataset
+from .ising import IsingDataset
+from .cahn_hilliard import CahnHilliardDataset
+from .chemical_elements import ChemicalElementsDataset
+from .nanoindentation import NanoindentationDataset
+
+__all__ = [
+    "IrisDataset",
+    "DigitsDataset",
+    "TensileTestDataset",
+    "IsingDataset",
+    "CahnHilliardDataset",
+    "ChemicalElementsDataset",
+    "NanoindentationDataset",
+]
